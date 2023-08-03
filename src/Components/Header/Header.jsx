09319c632089a96bc28cbd8ad102fd74d195d984
essi2002 <94480935+essi2002@users.noreply.github.com>
2023-08-03 03:40:25 +0330

    Home interface

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Header.css'
+import Navigation from '../Navigation/Navigation';
+
+
+
+
+
+function Header() {
+
+
+  return (
+    <div className="container-fluid main">
+
+  
+   <Navigation/>
+  
+  <div  className="carousel carousel-fade slide">
+    <div className="carousel-inner" role="listbox">
+    <div class="item active background a"></div>
+      <div class="item background b"></div>
+      <div class="item background c"></div>
+    </div>
+  </div>
+   
+  <div className="covertext">
+    <div className="col-lg-10" style={{ float: 'none', margin: '0 auto' }}>
+      <h1 style={{ fontStyle: 'italic' }} className="title">HexaCoffee</h1>
+      <h3 className="subtitle">A Tidy, Clean, Easy-to-Use, and Responsive coffee shop with Humble waiters</h3>
+    </div>
+    <div className="col-xs-12 explore">
+      <a href="#home"><button type="button" className="btn btn-lg explorebtn">Menu</button></a>
+    </div>
+  </div>
+  
+</div>
+  );
+}
+
+export default Header;
---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navigation.css'
+const Navigation = () => {
+    return ( 
+     
+      <nav>
+        <ul class="nav-type">
+          <li><a href="https://codepen.io/EneergeticTomy/"class="active">Home</a></li>
+          <li><a href="https://codepen.io/TomikaBoy/full/LqMpaE" class="active1">About</a></li>
+          <li><a href="https://codepen.io/"class="active2">Contact</a></li>
+          
+          <div class="line"></div>
+          
+        </ul>
+      </nav>
+
+     );
+}
+ 
+export default Navigation;
